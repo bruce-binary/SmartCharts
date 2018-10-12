ad35af2346a7aae5d75b0866a0b199c7b690e203
Lee Zhen Yong <bruce@binary.com>
2018-10-12 13:39:37 +0800

    update

--- binary-static/js/app_2/binary.js
+++ binary-static/js/app_2/binary.js
@@ -8079,7 +8079,7 @@ var _app2 = _interopRequireDefault(_app);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-(0, _mobx.configure)({ enforceActions: 'observed' });
+(0, _mobx.configure)({ enforceActions: true });
 
 var initApp = function initApp() {
     _client_base2.default.init();
